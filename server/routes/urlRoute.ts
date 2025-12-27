a665411f24610bb0465aec3a5b3f9ddb3cc6cd9f
Aditya Patil <adityas_mac@Adityas-MacBook-Air.local>
2025-12-27 12:42:33 +0530

    fixed cors

--- server/routes/urlRoute.ts
+++ server/routes/urlRoute.ts
@@ -1,11 +1,11 @@
 import { authenticate } from "../middlewares/authMiddleware.js";
-import { url, shorturl, redirectToOriginalUrl } from "../controllers/urlController";
+import { url, shorturl} from "../controllers/urlController";
 import express from "express";
 
 const router = express.Router();
 
 router.post("/url",authenticate,url);
 router.post("/shortenurl",authenticate,shorturl);
-router.get("/:shortcode",redirectToOriginalUrl)
+
 
 export default router;
